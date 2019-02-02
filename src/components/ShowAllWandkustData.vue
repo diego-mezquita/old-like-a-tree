9c3677cb8d615577916d75c6b52858fbbc84e59d
diego-mezquita <diegomezquitafreijoo@gmail.com>
2019-02-02 22:05:54 +0100

    create first custom component - ShowAllWandkustData just displays the data in wandkustData.features as a list

---
+++ src/components/ShowAllWandkustData.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+        <li v-for="data in $options.wandkustData.features" class="data-item">{{data}}</li>
+    </ul>
+</template>
+
+<script>
+    import WANDKUST_JSON from '../assets/data/wandkust.json'
+
+    export default {
+        name: "ShowAllWandkustData",
+        wandkustData: WANDKUST_JSON
+    }
+</script>
+
+<style scoped>
+.data-item {
+    padding: 2rem 0;
+}
+</style>
