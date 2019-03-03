c6b86d86a5c84d39ea48a3cc27820017960aa3f4
diego-mezquita <diegomezquitafreijoo@gmail.com>
2019-03-03 11:37:36 +0100

    change ui framework from semantic ui to vuetify

--- src/components/ShowAllMonumentaalGroenData.vue
+++ src/components/ShowAllMonumentaalGroenData.vue
@@ -1,6 +1,13 @@
 <template>
-    <ul>
-        <li v-for="data in $options.monumentaalGroenDatya.features" v-bind:key="data.id" class="data-item">{{data}}</li>
+    <ul class="ui sidebar inverted vertical menu list">
+        <li v-for="data in $options.monumentaalGroenData.features" v-bind:key="data.id" class="item">
+            <!--<a href="" class="item"></a>-->
+            <img class="ui avatar image" src="../assets/images/monument.svg">
+            <div class="content">
+                <p class="header">{{data.properties.Naam}}</p>
+                <p class="meta">{{data.properties.Status}}</p>
+            </div>
+        </li>
     </ul>
 </template>
 
@@ -14,7 +21,42 @@
 </script>
 
 <style scoped>
-.data-item {
-    padding: 2rem 0;
-}
 </style>
+
+
+<!--<div class="ui items">-->
+    <!--<div class="item">-->
+        <!--<div class="image">-->
+            <!--<img src="/images/wireframe/image.png">-->
+        <!--</div>-->
+        <!--<div class="content">-->
+            <!--<a class="header">Header</a>-->
+            <!--<div class="meta">-->
+                <!--<span>Description</span>-->
+            <!--</div>-->
+            <!--<div class="description">-->
+                <!--<p></p>-->
+            <!--</div>-->
+            <!--<div class="extra">-->
+                <!--Additional Details-->
+            <!--</div>-->
+        <!--</div>-->
+    <!--</div>-->
+    <!--<div class="item">-->
+        <!--<div class="image">-->
+            <!--<img src="/images/wireframe/image.png">-->
+        <!--</div>-->
+        <!--<div class="content">-->
+            <!--<a class="header">Header</a>-->
+            <!--<div class="meta">-->
+                <!--<span>Description</span>-->
+            <!--</div>-->
+            <!--<div class="description">-->
+                <!--<p></p>-->
+            <!--</div>-->
+            <!--<div class="extra">-->
+                <!--Additional Details-->
+            <!--</div>-->
+        <!--</div>-->
+    <!--</div>-->
+<!--</div>-->
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+// import '../semantic/src/semantic.less';
+// import 'semantic-ui-less/semantic.less';
+import basscss from 'basscss/css/basscss.min.css';
+
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+
+import Vuetify from 'vuetify';
+
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
