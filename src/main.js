7d9bbb781ffc53a31d327af48e22f53937082a09
diego-mezquita <diegomezquitafreijoo@gmail.com>
2019-03-12 20:02:57 +0100

    remove basscss import and comments

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-// import '../semantic/src/semantic.less';
-// import 'semantic-ui-less/semantic.less';
-import basscss from 'basscss/css/basscss.min.css';
-
 // index.js or main.js
 import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
